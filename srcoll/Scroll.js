ee6503ec3e47a4317b2837b95a393ebfbd326a5c
Dahee Lee <ai04120@naver.com>
2023-12-07 19:32:43 +0900

    ASDF

--- srcoll/Scroll.js
+++ srcoll/Scroll.js
@@ -60,3 +60,40 @@ document.addEventListener('scroll', () => {
     image.style.opacity = '0.5';
   }
 });
+
+// window.onload = function(){
+//   const elm = document.querySelectorAll('.scrollSection');
+//   const elmCount = elm.length;
+//   elm.forEach((item, index)=>{
+//     item.addEventListener('mousewheel', (event)=>{
+//       event.preventDefault();
+//       let delta = 0;
+
+//       if (!event) event = window.event;
+//       if (event.wheelDelta) {
+//           delta = event.wheelDelta / 120;
+//       } 
+//       else if (event.detail)
+//           delta = -event.detail / 3;
+
+//       let elmSelector = elm[index];
+
+//       if (delta < 0){
+//         if (elmSelector !== elmCount-1){
+//           try{
+//             window.scrollY = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+//           }catch(e){}
+//         }
+//       }
+
+//       else{
+//         if (elmSelector !== 0){
+//           try{
+//             window.scrollY = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+//           }catch(e){}
+//         }
+//       }
+//       window.scrollTo({top:window.scrollY, left:0, behavior:'smooth'});
+//     });
+//   });
+// }
