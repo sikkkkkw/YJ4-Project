a3d374894bdd0260186d59ba01e8406c9c69d16b
sikkkkkw <gksmsla92@naver.com>
2023-12-12 17:05:52 +0900

    test

--- popup/test.js
+++ popup/test.js
@@ -1,51 +1,42 @@
-// document.querySelector("#ptest1").addEventListener("click", function() {
-//     test1();
-// });
+document.addEventListener("DOMContentLoaded", function(){
+    // 1분(60,000밀리초) 후에 팝업을 띄우기 위해 setTimeout 사용
+    setTimeout(function() {
+        let pop = document.getElementById("stamp_layer");
+        pop.style.display = "block";
+    }, 60000); // 1분 후에 실행
+});
 
-// document.getElementById('Box').onclick = function() {
-//     test1();
-// }
+// 나머지 코드는 그대로 유지
 
-// function test1() {
-//     let url = "asd.html";
-//     let options ="width = 600, height = 400, ,menubar = no, toolbar = no, location = no, status = no, scrollbar = no";
-//     window.open( url, "", options);
-// }
 document.getElementById('open').onclick = function() {
     stampPopOpen();
 }
 
 function openPop() {
-    document.getElementById('popup_layer').style.display="block";
+    document.getElementById('popup_layer').style.display = "block";
 }
 
 function closePop() {
-    document.getElementById('popup_layer').style.display="none";
+    document.getElementById('popup_layer').style.display = "none";
 }
 
 function openPop2() {
-    document.getElementById('popup_layer2').style.display="block";
+    document.getElementById('popup_layer2').style.display = "block";
 }
 
 function closePop2() {
-    document.getElementById('popup_layer2').style.display="none";
+    document.getElementById('popup_layer2').style.display = "none";
 }
 
 function stampPopOpen() {
-    document.getElementById("stamp_layer").style.display="block";
-    document.getElementById("overlay").style.display="block";
-    
+    document.getElementById("stamp_layer").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
 }
 
 function stampPopClose() {
-    document.getElementById("stamp_layer").style.display="none";
-    document.getElementById("overlay").style.display="none";
+    document.getElementById("stamp_layer").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
-
-document.addEventListener("DOMContentLoaded", function(){
-    let pop = document.getElementById("stamp_layer");
-    pop.style.display = "block";
-})
 
 document.getElementById('eventpage').onclick = function() {
     pageOpen();
@@ -54,4 +45,19 @@ document.getElementById('eventpage').onclick = function() {
 function pageOpen() {
     let url = "eventpage.html";
     window.open(url, "_blank","     " )
+}
+
+function setCookie(name, value, expiredays) {
+    var todayDate = new Date();
+    todayDate.setTime(todayDate.getTime() + (expiredays * 60 * 1000)); // 1분으로 설정
+    document.cookie = name + "=" + escape(value) + "; path=/; expires=" + todayDate.toUTCString() + ";";
+}
+
+function PopupNoDisplay_1() {
+    var hideCheckbox = document.getElementById('hideCheckbox');
+
+    if (hideCheckbox.checked) {
+        setCookie("popupClosed", "done", 1); // 1분 후에 쿠키 만료
+        document.getElementById('stamp_layer').style.display = "none";
+    }
 }
